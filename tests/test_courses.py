a4cf1a7293af7000f367ab80f12125a7743944ec
skazakov <skazakov@rvision.ru>
2025-07-15 19:58:21 +0300

    6.5 Фикстуры Pytest.Практика работы с pytest фикстурами

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -4,7 +4,7 @@ from playwright.sync_api import expect
 @pytest.mark.regression
 @pytest.mark.courses
 
-def test_empty_courses_list1(chromium_page_with_state):
+def test_empty_courses_list(chromium_page_with_state):
     course = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
     expect(course).to_contain_text('Courses')
 
