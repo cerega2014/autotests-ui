ffae09be8fd0c263ddce5533e78ec55cfa47a8f1
skazakov <skazakov@rvision.ru>
2025-07-24 19:00:34 +0300

    6.7 Параметризация Pytest.Практика работы с pytest параметризацией

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
 from playwright.sync_api import Playwright, Page
 
+@pytest.fixture()
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    yield browser.new_page()
+    browser.close()
 
 @pytest.fixture(scope= 'session')
 def initialize_browser_state(playwright: Playwright) -> Page:
